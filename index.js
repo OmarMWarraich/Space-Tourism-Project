e572c053b94688975235f66932bec31b1c45476f
OmarMWarraich <earthling986@gmail.com>
2022-08-23 16:09:43 +0500

    style: Align grid-container items

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const body = document. querySelector('body');
 console.log(body);
 
@@ -5,15 +7,15 @@ const header = document.createElement('header');
 header.classList.add('primary-header');
 header.classList.add('flex');
 
-header.innerHTML = `<div class="img">
+header.innerHTML = `<div class="logo">
                         <img src="./assets/nav_logo.svg" alt="Space Logo">
                     </div>
                     <nav>
                         <ul class="primary-navigation underline-indicators flex">
-                            <li class="active"><a class="ff-sans-cond uppercase tex-white letter-spacing-2" href="index.html"><span>00</span>Home</a></li>
-                            <li><a class="ff-sans-cond uppercase tex-white letter-spacing-2" href="destination.html"><span>01</span>Destination</a></li>
-                            <li><a class="ff-sans-cond uppercase tex-white letter-spacing-2" href="crew.html"><span>02</span>Crew</a></li>
-                            <li><a class="ff-sans-cond uppercase tex-white letter-spacing-2" href="technology.html"><span>03</span>Technology</a></li>
+                            <li class="active"><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="index.html"><span>00</span>Home</a></li>
+                            <li><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="destination.html"><span>01</span>Destination</a></li>
+                            <li><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="crew.html"><span>02</span>Crew</a></li>
+                            <li><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="technology.html"><span>03</span>Technology</a></li>
                         </ul>
                     </nav>
                     `;
@@ -21,15 +23,23 @@ header.innerHTML = `<div class="img">
 body.appendChild(header);
 
 const container = document.createElement('div');
-container.classList.add('container');
+container.classList.add('grid-container');
 
 container.innerHTML = `
-                    <h1 class="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">
-                    So, you want to travel to<span class="fs-900 ff-serif text-white">Space</span></h1>
-                    <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
-                    outer space and not hover kind of on the edge of it. Well sit back, and relax 
-                    because we’ll give you a truly out of this world experience!</p>
-                    <a href="#" class="large-button upper-case ff-serif fs-900 text-dark bg-white">Explore</a> 
+                    <div>
+                        <h1 class="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">
+                            So, you want to travel to
+                        <span class="fs-900 ff-serif text-white">Space</span>
+                        </h1>
+                        <p>
+                            Let’s face it; if you want to go to space, you might as well genuinely go to 
+                            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                            because we’ll give you a truly out of this world experience!
+                        </p>
+                    </div>
+                    <div>
+                        <a href="#" class="large-button uppercase ff-serif fs-600 text-dark bg-white">Explore</a> 
+                    </div>
                     `
 
 body.appendChild(container);
